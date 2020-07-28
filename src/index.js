5db334f89c05355cc6e8c1e426f5fdbb120b74f6
unknown <you@example.com>
2020-07-27 21:16:24 -0300

    yarn start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(port, () => {
-    console.log("server running in localhost:3333")
-} 
-
 const port = 3333;
+
+app.listen(port, () => {
+  console.log(`server running in localhost:${port}`);
+});
