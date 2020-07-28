e6a40f4a2d441e18bdaa834bd8dcd3edce0d4050
unknown <you@example.com>
2020-07-27 22:09:10 -0300

    Add methods GET, PUSH, PUT

--- src/index.js
+++ src/index.js
@@ -8,13 +8,40 @@ app.use(express.json());
 // array DB
 const scraps = [
   {
-    nome: "teste",
-    mensagem: "teste",
+    name: "teste",
+    message: "teste",
   },
 ];
 // routes
 app.get("/", (req, res) => {
   return scraps;
+});
+
+app.get("/:name", (req, res) => {
+  const { name } = req.params;
+
+  const scrapIndex = scraps.findIndex((scrap) => scrap.name == name);
+
+  return scraps[scrapIndex];
+});
+
+app.post("/", (req, res) => {
+  const { title, owner } = request.body;
+  const scraps = { id: uuid(), title, owner };
+
+  scraps.push(project);
+});
+
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { title, owner } = req.body;
+
+  const scrapIndex = scraps.findIndex((scrap) => scrap.id === id);
+
+  if (scrapIndex < 0) {
+    return response.status(400).json({ error: "Scrap not found." });
+  }
 });
 
 /// server port
