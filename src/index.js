ebc57c8c259ac7f9fb5e1c928bfd06c48fee1121
unknown <you@example.com>
2020-07-27 21:28:49 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+/// dependencies
 const express = require("express");
 const app = express();
+const { uuid } = require("uuidv4");
 
+app.use(express.json());
+
+// array DB
+const scraps = [
+  {
+    nome: "teste",
+    mensagem: "teste",
+  },
+];
+// routes
+app.get("/", (req, res) => {
+  return scraps;
+});
+
+/// server port
 const port = 3333;
-
 app.listen(port, () => {
   console.log(`server running in localhost:${port}`);
 });
